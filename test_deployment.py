b847ed4822e7d81440e61c282fcdea0541e23eec
fkischewski99 <fkischewski99@gmail.com>
2021-03-13 15:48:11 +0100

    refactor IV

--- test_deployment.py
+++ test_deployment.py
@@ -11,37 +11,48 @@ from telethon import TelegramClient
 key = paramiko.RSAKey.from_private_key_file(config["core"]["key"])
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-api_id = config['telegram']['api_id']
-api_hash = config['telegram']['api_hash']
+# api_id = config['telegram']['api_id']
+# api_hash = config['telegram']['api_hash']
 
-bot = TelegramClient('aws', api_id, api_hash)
+# bot = TelegramClient('aws', api_id, api_hash)
 ec2_client = EC2Client().get_client()
 vpc = VPC(ec2_client)
 ec2 = EC2(ec2_client)
 ssh = AWSSSH(client)
 
-async def main():
-    await bot.send_message('billaids', 'Hello!')
+
+def main():
+    #    await bot.send_message('billaids', 'Hello!')
     # ssh.exec_cmd(
     #     cmd="du -hcs .",
     #     key=key,
     #     host=config["core"]["server_ip"],
     #     user=config["core"]["username"],
     # )
-    
+
     # Credential check
     logger.info(boto3.Session().get_credentials().access_key)
     logger.info(boto3.Session().get_credentials().secret_key)
 
     vpc_check = ec2.check_vpc(config["vpc"]["vpc_name"])
-    if not vpc_check:
-        vpc._set_vpc_id_(vpc_check)
-        vpc_id = vpc_check
+    if vpc_check:
+        vpc_id = vpc_check[0]["VpcId"]
+        vpc._set_vpc_id_(vpc_id)
     else:
         # Create VPC
         vpc_id = vpc.create_vpc(
             cidr=config["vpc"]["vpc_cidr_block"], vpc_name=config["vpc"]["vpc_name"]
         )
+
+    public_subnet = ec2.check_subnet(config["subnet"]["public"]["tag"])
+    private_subnet = ec2.check_subnet(config["subnet"]["private"]["tag"])
+
+    if public_subnet and private_subnet:
+        logger.info(public_subnet[0])
+        private_subnet_id = private_subnet[0]["SubnetId"]
+        public_subnet_id = public_subnet[0]["SubnetId"]
+
+    else:
         # Create Public Subnet
         public_subnet_id = vpc.init_subnet(
             cidr=config["subnet"]["public"]["cidr"],
@@ -58,7 +69,9 @@ async def main():
         igw_id = vpc.init_igw()
 
         # Create NAT Gateway
-        nat_gateway_id = vpc.create_nat_gateway(subnet_id=public_subnet_id, gateway_tag=config['core']['nat_gateway_name'])
+        nat_gateway_id = vpc.create_nat_gateway(
+            subnet_id=public_subnet_id, gateway_tag=config["core"]["nat_gateway_name"]
+        )
 
         # Create public route table
         public_route_table_id = vpc.init_route_table()
@@ -84,6 +97,18 @@ async def main():
         # Allow auto assign IP for public subnet
         vpc.allow_auto_assign_ip_addresses_for_subnet(subnet_id=public_subnet_id)
 
+    public_security_group = ec2.describe_security_groups(
+        config["secgroup"]["public"]["name"]
+    )
+    private_security_group = ec2.describe_security_groups(
+        config["secgroup"]["private"]["name"]
+    )
+
+    if public_security_group and private_security_group:
+        logger.info(private_security_group)
+        public_security_group_id = public_security_group["GroupId"]
+        private_security_group_id = private_security_group["GroupId"]
+    else:
         # Security Gruppen festlegen & Route Tables festlegen
         public_security_group = ec2.create_security_group(
             group_name=config["secgroup"]["public"]["name"],
@@ -99,6 +124,9 @@ async def main():
         public_security_group_id = public_security_group["GroupId"]
         private_security_group_id = private_security_group["GroupId"]
 
+        # Create Tags for security groups
+        ec2._client.create_tags(Resources=[public_security_group_id], Tags=[{'Key': 'Name', 'Value': config["secgroup"]["public"]["name"]}])
+        ec2._client.create_tags(Resources=[private_security_group_id], Tags=[{'Key': 'Name', 'Value': config["secgroup"]["private"]["name"]}])
         # Add Public IP_permission Rules
         ec2.add_inbound_rule_to_sg(
             public_security_group_id,
@@ -165,6 +193,8 @@ async def main():
 
     # TODO: Alle IPs von Worker-Nodes herausfinden und Deploy-Command schreiben
 
+
 if __name__ == "__main__":
-    with bot:
-        bot.loop.run_until_complete(main())
+    # with bot:
+    #    bot.loop.run_until_complete(main())
+    main()
