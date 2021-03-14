40f8f89785034308d971dfa61b6f5eee2d82dc1f
billaids <jimmy.nelle@hsw-stud.de>
2021-03-14 12:17:25 +0100

    refactor VI & cleanup
    * add sftp
    * add auto configs for worker nodes
    * create and import keypairs
    * run commands on master & worker nodes
    
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>

--- helpers/ssh.py
+++ helpers/ssh.py
@@ -24,3 +24,24 @@ class AWSSSH:
 
         except Exception as e:
             logger.error(e)
+
+    def config_all(self, key, host, user, ip):
+        self.exec_cmd(
+            cmd=f"bash deploy_all.sh {ip}",
+            key=key,
+            host=host,
+            user=user,
+        )
+
+    def upload_sftp(self, localpath, remotepath, host, user, key):
+        try:
+            self._ssh.connect(hostname=host, username=user, pkey=key)
+            sftp = self._ssh.open_sftp()
+            # https://docs.paramiko.org/en/stable/api/sftp.html#paramiko.sftp_client.SFTPClient.put
+            sftp.put(localpath, remotepath, confirm=True)
+            time.sleep(1)
+            logger.info(f"Successfully upload file {localpath}")
+            self._ssh.close()
+
+        except Exception as e:
+            logger.error(e)
